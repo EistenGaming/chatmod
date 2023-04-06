c3b80276ddd1dcf2c5987f491326c18b34e2bb75
Michael Boeni <zen@eistengaming.com>
2023-04-06 14:39:49 +0200

    [Added] code to loop the user entry prompt until the exit commands '/quit' or '/exit' are used.

--- chatmod.go
+++ chatmod.go
@@ -8,15 +8,27 @@ import (
 )
 
 func main() {
+	/* Initialize the API to be used */
 	client := gptInit()
+	/* Initialize the userInput variable. This is what the user enters on the keyboard */
 	var userInput string
-	fmt.Print("> ")
-	inputReader := bufio.NewReader(os.Stdin)
-	userInput, _ = inputReader.ReadString('\n')
-	userInput = strings.TrimSuffix(userInput, "\n")
 
-	fmt.Println("The question was: " + userInput)
+	/* Loop until the command '/quit' or '/exit' is typed */
+	for {
 
-	fmt.Println(gptComplete(client, userInput))
+		fmt.Print("> ")
+		inputReader := bufio.NewReader(os.Stdin)
+		userInput, _ = inputReader.ReadString('\n')
+		userInput = strings.TrimSuffix(userInput, "\n")
+
+		/* Check for the exit condition */
+		if userInput == "/quit" || userInput == "/exit" {
+			break
+		}
+
+		fmt.Println("The question was: " + userInput)
+		fmt.Println(gptComplete(client, userInput))
+
+	}
 
 }
