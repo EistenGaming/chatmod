c0b51657285addb50aad4ee0e894f55a59aaf786
Michael Boeni <zen@eistengaming.com>
2023-04-10 21:35:15 +0200

    [Added] A function to generate an image using DALL-E.

--- chatlib.go
+++ chatlib.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/base64"
 	"fmt"
+	"image/png"
 	"os"
 
 	openai "github.com/sashabaranov/go-openai"
@@ -36,4 +39,67 @@ func gptComplete(client *openai.Client, msg string) string {
 	}
 
 	return resp.Choices[0].Message.Content
+}
+
+// ASk DALL-E to generate an image from a prompt
+func dalleGenerate(client *openai.Client, msg string) {
+
+	ctx := context.Background()
+
+	// Sample image by link
+	reqUrl := openai.ImageRequest{
+		Prompt:         msg,
+		Size:           openai.CreateImageSize1024x1024,
+		ResponseFormat: openai.CreateImageResponseFormatURL,
+		N:              1,
+	}
+
+	respUrl, err := client.CreateImage(ctx, reqUrl)
+	if err != nil {
+		fmt.Printf("Image creation error: %v\n", err)
+		return
+	}
+	fmt.Println(respUrl.Data[0].URL)
+
+	// Example image as base64
+	reqBase64 := openai.ImageRequest{
+		Prompt:         msg,
+		Size:           openai.CreateImageSize1024x1024,
+		ResponseFormat: openai.CreateImageResponseFormatB64JSON,
+		N:              1,
+	}
+
+	respBase64, err := client.CreateImage(ctx, reqBase64)
+	if err != nil {
+		fmt.Printf("Image creation error: %v\n", err)
+		return
+	}
+
+	imgBytes, err := base64.StdEncoding.DecodeString(respBase64.Data[0].B64JSON)
+	if err != nil {
+		fmt.Printf("Base64 decode error: %v\n", err)
+		return
+	}
+
+	r := bytes.NewReader(imgBytes)
+	imgData, err := png.Decode(r)
+	if err != nil {
+		fmt.Printf("PNG decode error: %v\n", err)
+		return
+	}
+
+	file, err := os.Create("example.png")
+	if err != nil {
+		fmt.Printf("File creation error: %v\n", err)
+		return
+	}
+	defer file.Close()
+
+	if err := png.Encode(file, imgData); err != nil {
+		fmt.Printf("PNG encode error: %v\n", err)
+		return
+	}
+
+	fmt.Println("The image was saved as example.png")
+
 }
