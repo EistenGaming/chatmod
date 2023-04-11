f2924c0ce9d3e758608eb9997170d1884c8b3842
Michael Boeni <zen@eistengaming.com>
2023-04-11 23:40:05 +0200

    [Modified] the help text. Added examples and improved formatting.

--- chatmod.go
+++ chatmod.go
@@ -14,13 +14,30 @@ func main() {
 	var userInput string
 
 	/* A string listing all valid commands */
-	var helpString = "\n/complete \t\tUse the complete function of an LLM. This is stateless, i.e. you can not reference a preceding prompt.\n/dalleGenerate \t\tGenerate an image using DALL-E. The image is saved as 'example.png' in the current directory.\n/exit \t\t\tTerminate the app.\n/help \t\t\tShow this help text.\n/quit \t\t\t(see /exit)\n"
+	var helpString = "\n" +
+		"Chatmod is a tool for testing LLM models and other generative systems.\n" +
+		"\n" +
+		"In most cases, you need to type a command, followed by a prompt.\n" +
+		"\n\n" +
+		"Examples:" +
+		"\n\n" +
+		"\t/complete Write a 200 word essay about iron prospecting." +
+		"\n\n" +
+		"\t/dalleGenerate Create an image of a red children's ball with white dots in front of a yellow wall." +
+		"\n\n\n" +
+		"Available commands:" +
+		"\n\n" +
+		"\t/complete [Prompt] \t\tUse the complete function of an LLM. This is stateless, a preceding prompt is not taken into consideration.\n" +
+		"\t/dalleGenerate [Prompt] \tGenerate an image using DALL-E. The image is saved as 'image_[Unique ID].png' in the current directory.\n" +
+		"\t/exit \t\t\t\tTerminate the app.\n" +
+		"\t/help \t\t\t\tShow this help text.\n" +
+		"\t/quit \t\t\t\t(see /exit)\n"
 
 	/* Loop until the command '/quit' or '/exit' is typed */
 	for {
 
-		/* Show the valid commands at startup */
-
+		/* Show the welcome text */
+		fmt.Print("\nWelcome to chatmod. Type '/help' for available commands.\n")
 		/* Show the prompt */
 		fmt.Print("> ")
 		inputReader := bufio.NewReader(os.Stdin)
@@ -33,7 +50,6 @@ func main() {
 			var tokens = strings.Fields(userInput)
 			if len(tokens[0]) > 0 {
 				command = tokens[0]
-				fmt.Println("[DEBUG] Selected command: " + command)
 				/* remove the command token from the user input as it is not intended to be sent to the API */
 				userInput = strings.TrimLeft(userInput, command)
 			}
